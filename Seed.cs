ac27790e363c71eff8030202dfdbe2daec52019a
sabitalizade <sabitalizade@gmail.com>
2024-01-10 18:11:44 +0400

    Seed created

---
+++ Seed.cs
@@ -0,0 +1,46 @@
+
+
+using BooksApi.Data;
+using BooksApi.Models;
+
+namespace BooksApi
+{
+    public class Seed
+    {
+        private readonly DataContext dataContext;
+        public Seed(DataContext context)
+        {
+            this.dataContext = context;
+        }
+        public void SeedDataContext()
+        {
+            if (!dataContext.StoreBooks.Any())
+            {
+                var storeBooks = new List<StoreBooks>()
+                {
+                    new StoreBooks()
+                    {
+
+                        Books = new Book()
+                        {
+                            Title = "The Way of Kings",
+                            Genre = "Fantasy",
+                            Price = 10,
+                            Author = new Author()
+                            {
+                                FirstName = "Brandon",
+                                LastName = "Sanderson"
+                            }
+                        },
+                        Stores = new Stores()
+                        {
+                            Name = "Amazon"
+                        }
+                    },
+                };
+                dataContext.StoreBooks.AddRange(storeBooks);
+                dataContext.SaveChanges();
+            }
+        }
+    }
+}
