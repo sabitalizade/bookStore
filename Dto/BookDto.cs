c833c5954f15bbe2ff4b3832993ededb6c1316de
sabitalizade <sabitalizade@gmail.com>
2024-01-10 18:09:23 +0400

    Dtos added

---
+++ Dto/BookDto.cs
@@ -0,0 +1,10 @@
+namespace BookStore.Dtos
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Genre { get; set; }
+        public int? Price { get; set; }
+    }
+}
---
+++ Dto/StoreDto.cs
@@ -0,0 +1,8 @@
+namespace BookStore.Dtos
+{
+    public class StoreDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
