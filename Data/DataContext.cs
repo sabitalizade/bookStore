1c6fb93268021ea0e1aaf21d092c34f210e97bd0
sabitalizade <sabitalizade@gmail.com>
2024-01-10 18:11:00 +0400

    Data context created

---
+++ Data/DataContext.cs
@@ -0,0 +1,31 @@
+using BooksApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<StoreBooks> StoreBooks { get; set; }
+        public DbSet<Stores> Stores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StoreBooks>()
+                .HasKey(c => new { c.BookId, c.StoreId });
+
+            modelBuilder.Entity<StoreBooks>()
+                .HasOne(bc => bc.Books)
+                .WithMany(b => b.StoreBooks)
+                .HasForeignKey(bc => bc.BookId);
+
+            modelBuilder.Entity<StoreBooks>()
+                .HasOne(bc => bc.Stores)
+                .WithMany(c => c.StoreBooks)
+                .HasForeignKey(bc => bc.StoreId);
+
+        }
+    }
+}
